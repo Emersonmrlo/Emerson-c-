43e8bdd87a661bacb9f6b231e9218bd5213114d3
Emersonmrlo <128187940+Emersonmrlo@users.noreply.github.com>
2023-03-18 21:01:07 -0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,28 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //Digite um programa que exiba a qrea  de um triangulo 
+ float b,h;/*Ou float b,h,r assim evintando o inteiro para numeros quabrados ex:7.5*/
+ int r;
+ cout<<"Digite a base e altura do triangulo"<<"\n";
+ cin>>b>>h;
+ 
+ r=(b*h)/2;
+ 
+ cout<<"A area do triangulo é"<<r<<"\n";
+ 
+
+    return 0;
+}
+
